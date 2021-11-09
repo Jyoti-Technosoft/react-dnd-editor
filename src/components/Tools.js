4c60b56d2cf06d1da6249058d64ed8fd984a6d0b
dipesh <dipesh_gandhi9@yahoo.com>
2021-11-09 18:46:35 +0530

    Add header, tools and editor container

---
+++ src/components/Tools.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Tools extends Component {
+    render() {
+        return (
+            <div className="">
+                <li>
+                    Text
+                </li>
+                <li>
+                    Object
+                </li>
+            </div>
+        )
+    }
+}
+
+export default Tools;
